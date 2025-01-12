ca3718eb9ea11fa49ec1c81fcb4d33739dff65e5
John Dalbey <jdalbey@calpoly.edu>
2025-01-12 10:49:14 -0800

    appconfig.py Add more preference options and remove required filepath.  Add more tests to test_appconfig.py.

--- src/appconfig.py
+++ src/appconfig.py
@@ -1,104 +1,49 @@
-import configparser
+from configparser import ConfigParser
 import os
 
 class AppConfig:
-    """Application configuration settings"""
+    """Application configuration settings (Preferences)"""
     _instance = None
-
-    def __new__(cls, config_file=None):
+    kDefaultPath = ".config/EasyAuth/config.ini"
+    def __new__(cls, filepath=None):
         if cls._instance is None:
             cls._instance = super(AppConfig, cls).__new__(cls)
-            cls._instance._config_file = config_file
-            cls._instance._config = configparser.ConfigParser()
-            if config_file:
-                cls._instance.read(config_file)
-            else:
-                cls._instance._config['settings'] = cls.get_default_settings()
+            cls._instance._initialized = False
         return cls._instance
 
-    @staticmethod
-    def get_default_settings():
-            return  {
-                'vault_path': 'default.db',
-                'smart_filtering': 'False',
-                'theme_name': 'default',
-                'alt_id': '',
-                'auto_find_qr': 'True'
-            }
+    def __init__(self, filepath=None):
+        if self._initialized:
+            return
+        # This code executes just once for first/only instance
+        self.config = ConfigParser()
+        self.filepath = filepath
+        # If a filepath was provided load settings from there
+        if filepath:
+            self.read(filepath)
+        # otherwise fallback settings will be used by restore_defaults
+        else:
+            self.filepath = self.kDefaultPath
+            self.restore_defaults()
+        self._initialized = True
 
     def read(self, config_file):
+        """ Read config file or create it with defaults """
+        # does the file exist
         if not os.path.exists(config_file):
             print (f"The configuration file '{config_file}' does not exist, creating it.")
+            # is the directory missing?
             if not os.path.exists(os.path.dirname(config_file)):
-                os.makedirs(os.path.dirname(config_file))
-                with open(config_file, 'w') as file:
-                    self._config['settings'] = self.get_default_settings()
-                    self._config.write(file)
-                    print("saved config file")
+                os.makedirs(os.path.dirname(config_file),True)
+            # write defaults to the file
+            self.restore_defaults()
+            print("restored defaults and saved to config file")
         else:
-            self._config.read(config_file)
-            print ("Read config file:")
-            print (self._config.sections())
+            self.config.read(config_file)
 
-    def get_vault_path(self):
-        """
-        Get the path to the application database.
-        """
-        return self._config.get('settings', 'vault_path', fallback='default.db')
-
-    def set_vault_path(self, path):
-        """
-        Set the path to the application database.
-        """
-        self._config.set('settings', 'vault_path', path)
-
-    def is_smart_filtering_enabled(self):
-        """
-        Check if smart filtering is enabled.
-        """
-        return self._config.getboolean('settings', 'smart_filtering', fallback=False)
-
-    def set_smart_filtering_enabled(self, enabled):
-        """
-        Set the smart filtering enabled status.
-        """
-        self._config.set('settings', 'smart_filtering', str(enabled))
-
-    def get_theme_name(self):
-        """
-        Get the name of the theme.
-        """
-        return self._config.get('settings', 'theme_name', fallback='default')
-
-    def set_theme_name(self, theme_name):
-        """
-        Set the name of the theme.
-        """
-        self._config.set('settings', 'theme_name', theme_name)
-
-    def get_alt_id(self):
-        """
-        Get the alternate id.
-        """
-        return self._config.get('settings', 'alt_id', fallback=None)
-
-    def set_alt_id(self, alt_id):
-        """
-        Set the alternate id.
-        """
-        self._config.set('settings', 'alt_id', alt_id)
-
-    def is_auto_find_qr_enabled(self):
-        """
-        Check if auto find QR code is enabled.
-        """
-        return self._config.getboolean('settings', 'auto_find_qr', fallback=False)
-
-    def set_auto_find_qr_enabled(self, enabled):
-        """
-        Set the auto find QR code enabled status.
-        """
-        self._config.set('settings', 'auto_find_qr', str(enabled))
+    def saveconfig(self):
+        if self.filepath:
+            with open(self.filepath, 'w') as configfile:
+                self.config.write(configfile)
 
     def reload(self, config_file):
         """
@@ -111,10 +56,133 @@ class AppConfig:
         Reload the configuration from a string (INI format).
         Dependency injection for testing.
         """
-        self._config.read_string(config_string)
+        self.config.read_string(config_string)
 
-    def save_config(self):
-        print ("Start saving config file")
-        if self._instance._config_file:
-            with open(self._instance._config_file, 'w') as configfile:
-                self._config.write(configfile)
+    def get(self, section, option, fallback=None):
+        return self.config.get(section, option, fallback=fallback)
+
+    def set(self, section, option, value):
+        """ Set an options value and write it """
+        if not self.config.has_section(section):
+            self.config.add_section(section)
+        self.config.set(section, option, value)
+        self.saveconfig()
+
+    # Add methods to get and set the new settings
+    def is_auto_find_qr_enabled(self):
+        return self.get('Settings', 'auto_find_qr', fallback='False') == 'True'
+
+    def set_auto_find_qr_enabled(self, value):
+        self.set('Settings', 'auto_find_qr', 'True' if value else 'False')
+
+    def get_search_by(self):
+        return self.get('Settings', 'search_by', fallback='Provider')
+
+    def set_search_by(self, value):
+        self.set('Settings', 'search_by', value)
+
+    def is_minimize_after_copy(self):
+        return self.get('Settings', 'minimize_after_copy', fallback='False') == 'True'
+
+    def set_minimize_after_copy(self, value):
+        self.set('Settings', 'minimize_after_copy', 'True' if value else 'False')
+
+    def is_minimize_during_qr_search(self):
+        return self.get('Settings', 'minimize_during_qr_search', fallback='False') == 'True'
+
+    def set_minimize_during_qr_search(self, value):
+        self.set('Settings', 'minimize_during_qr_search', 'True' if value else 'False')
+
+    def is_auto_fetch_favicons(self):
+        return self.get('Settings', 'auto_fetch_favicons', fallback='False') == 'True'
+
+    def set_auto_fetch_favicons(self, value):
+        self.set('Settings', 'auto_fetch_favicons', 'True' if value else 'False')
+
+    def is_display_favicons(self):
+        return self.get('Settings', 'display_favicons', fallback='False') == 'True'
+
+    def set_display_favicons(self, value):
+        self.set('Settings', 'display_favicons', 'True' if value else 'False')
+
+    def is_secret_key_hidden(self):
+        return self.get('Settings', 'secret_key_hidden', fallback='False') == 'True'
+
+    def set_secret_key_hidden(self, value):
+        self.set('Settings', 'secret_key_hidden', 'True' if value else 'False')
+
+    def get_language(self):
+        return self.get('Settings', 'language', fallback='English')
+
+    def set_language(self, value):
+        self.set('Settings', 'language', value)
+
+    def is_animate_copy(self):
+        return self.get('Settings', 'animate_copy', fallback='False') == 'True'
+
+    def set_animate_copy(self, value):
+        self.set('Settings', 'animate_copy', 'True' if value else 'False')
+
+    def get_vault_path(self):
+        """
+        Get the path to the application database.
+        """
+        return self.config.get('Settings', 'vault_path', fallback='vault.json')
+
+    def set_vault_path(self, path):
+        """
+        Set the path to the application database.
+        """
+        self.config.set('Settings', 'vault_path', path)
+
+    def is_smart_filtering_enabled(self):
+        """
+        Check if smart filtering is enabled.
+        """
+        return self.config.getboolean('Settings', 'smart_filtering', fallback=False)
+
+    def set_smart_filtering_enabled(self, enabled):
+        """
+        Set the smart filtering enabled status.
+        """
+        self.config.set('Settings', 'smart_filtering', str(enabled))
+
+    def get_theme_name(self):
+        """
+        Get the name of the theme.
+        """
+        return self.config.get('Settings', 'theme_name', fallback='light')
+
+    def set_theme_name(self, theme_name):
+        """
+        Set the name of the theme.
+        """
+        self.config.set('Settings', 'theme_name', theme_name)
+
+    def get_alt_id(self):
+        """
+        Get the alternate id.
+        """
+        return self.config.get('Settings', 'alt_id', fallback="")
+
+    def set_alt_id(self, alt_id):
+        """
+        Set the alternate id.
+        """
+        self.config.set('Settings', 'alt_id', alt_id)
+
+    def restore_defaults(self):
+        """ restore and write defaults """
+        self.set_auto_find_qr_enabled(False)
+        self.set_search_by('Provider')
+        self.set_minimize_after_copy(False)
+        self.set_minimize_during_qr_search(False)
+        self.set_auto_fetch_favicons(False)
+        self.set_display_favicons(False)
+        self.set_secret_key_hidden(False)
+        self.set_language('English')
+        self.set_animate_copy(False)
+        self.set_vault_path("vault.json")
+        self.set_smart_filtering_enabled(False)
+        self.set_theme_name("light")
+        self.set_alt_id("")
--- tests/test_appconfig.py
+++ tests/test_appconfig.py
@@ -1,12 +1,22 @@
 # main.py
+import os
+
 from appconfig import AppConfig
 from pathlib import Path
 
 class TestAppConfig:
 
     def test_appconfig(self):
-        # Initialize the singleton instance with a config file
-        kConfigPath = ".config/EasyAuth/config.ini"
+        # reset the singleton
+        AppConfig._instance = None
+        # A complete config file
+        settings_string = """[Settings]\nauto_find_qr = False\nsearch_by = Provider\nminimize_after_copy = False
+        minimize_during_qr_search = False\nauto_fetch_favicons = False\ndisplay_favicons = False\nsecret_key_hidden = False
+        language = English\nanimate_copy = False\nvault_path = vault.json"""
+        kConfigPath = "/tmp/config_test.ini"
+        text_file = open(kConfigPath, "w")
+        text_file.write(settings_string)
+        text_file.close()
         home_dir_str = str(Path.home())
         filepath = Path.home().joinpath(home_dir_str, kConfigPath)
         config = AppConfig(filepath)
@@ -17,10 +27,12 @@ class TestAppConfig:
         alt_id = config.get_alt_id()
 
         assert vault_path == "vault.json"
-        assert smart_filter == True
-        assert theme == "dark"
-        assert alt_id.startswith("10b")
+        assert smart_filter == False
+        assert theme == "light"
+        #assert alt_id.startswith("10b")
+        assert alt_id == ""
 
+        # Test the set config from string method
         config_string = """
         [settings]
         database_path = /path/to/database.db
@@ -30,4 +42,55 @@ class TestAppConfig:
         """
         config.set_config(config_string)
         assert config.get_theme_name() == "light"
-        assert config.get_alt_id() == "A1B2"
+
+    def test_partial_config(self):
+        # reset the singleton
+        AppConfig._instance = None
+        # A config file with missing setting for vault_path
+        settings_string = """[Settings]\nauto_find_qr = False\nsearch_by = Provider\nminimize_after_copy = False
+minimize_during_qr_search = False\nauto_fetch_favicons = False\ndisplay_favicons = False\nsecret_key_hidden = False
+language = English\nanimate_copy = False\n"""
+        kConfigPath = "/tmp/config_partial_test.ini"
+        text_file = open(kConfigPath, "w")
+        text_file.write(settings_string)
+        text_file.close()
+        home_dir_str = str(Path.home())
+        filepath = Path.home().joinpath(home_dir_str, kConfigPath)
+        config = AppConfig(filepath)
+
+        vault_path = config.get_vault_path()
+        assert vault_path == "vault.json"
+
+    def test_missing_config(self):
+        # reset the singleton
+        AppConfig._instance = None
+        kConfigPath = "/tmp/config_missing.ini"
+        try:
+            os.remove(kConfigPath)
+        except FileNotFoundError as e:
+            pass
+        home_dir_str = str(Path.home())
+        filepath = Path.home().joinpath(home_dir_str, kConfigPath)
+        config = AppConfig(filepath)
+
+        vault_path = config.get_vault_path()
+        assert vault_path == "vault.json"
+
+    def test_noarg(self):
+        # reset the singleton
+        AppConfig._instance = None
+        # use a temp file for testing
+        filepath = "/tmp/default_config.ini"
+        try:
+            os.remove(filepath)
+        except FileNotFoundError as e:
+            pass
+        AppConfig.kDefaultPath = filepath
+
+        config = AppConfig()  # noarg constructor
+
+        vault_path = config.get_vault_path()
+        assert vault_path == "vault.json"
+        # should have been created by restore_defaults()
+        assert os.path.exists(filepath)
+
