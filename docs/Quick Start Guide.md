5053cf2d70f1f4424d3bbe81a8ae9c03fb07934d
John Dalbey <jdalbey@calpoly.edu>
2025-02-01 06:57:12 -0800

    Update quick_start_dialog.py to use appropriate path when bundled in pyinstaller.

--- docs/Quick Start Guide.md
+++ docs/Quick Start Guide.md
@@ -10,20 +10,20 @@ The quide will show how to store shared keys in your EasyAuth vault and use Time
 ### Step 1: Create an account in EasyAuth
 
 1. Open EasyAuth.  The first time it is launched a message indicates that you have an empty vault.  
-![Empty vault screenshot](https://i.ibb.co/NdZ7g6Qv/vault-empty.png)
+
 2. Make sure the QR code is visible somewhere on the screen. Click the 'Add Account' button. 
 The program will search for a visible QR code and the QR code data will be entered into the form fields.  
-![Add Account Form screenshot](https://i.ibb.co/8gCSrhcw/Add-Account-Form.png)
+
 3. You may change the value of the User field to any label that reminds you which online account this entry is associated with (usually username or email address).  
 4. Click "Add." The dialog closes and the main window appears with the new account listed at the top.   The Provider and User fields are shown on the left and on the right is the TOTP.  
-![Main Window screenshot](https://i.ibb.co/1t35DGdJ/Main-Window.png)
+
 
 
 
 ### Step 2: Verifying with the TOTP code.
 
 1. Now that EasyAuth has created an account for the desired website, it  will display a six-digit One-Time Password (TOTP) for the account that  changes every 30 seconds. The seconds timer indicates how much  time remains for the displayed TOTP to be active.  
-![TOTP code](https://i.ibb.co/W4gGx8Hf/TOTP-code.png)
+
 2. Click the 6-digit TOTP code to copy it to the clipboard.
 
 3. Complete the setup process on the website by pasting this code into the verify field in your browser. 
--- src/quick_start_dialog.py
+++ src/quick_start_dialog.py
@@ -1,37 +1,41 @@
 import os
 import sys
+import logging
 
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QTextEdit, QDialogButtonBox
-
 
 class QuickStartDialog(QDialog):
     def __init__(self,parent):
         super().__init__(parent)
+        self.logger = logging.getLogger(__name__)
+
         self.setWindowTitle("Quick Start Guide")
         self.resize(700,550)
         layout = QVBoxLayout()
 
-        logOutput = QTextEdit(self)
-        logOutput.setReadOnly(True)
-        logOutput.setLineWrapMode(QTextEdit.WidgetWidth)
+        display_window = QTextEdit(self)
+        display_window.setReadOnly(True)
+        display_window.setLineWrapMode(QTextEdit.WidgetWidth)
         content = self.load_quickstart_text()
         if len(content) > 0:
-            logOutput.setHtml(content)
+            display_window.setHtml(content)
         else:
-            logOutput.setHtml("<h2>Quick Start guide</h2> Appearing here soon.")
+            display_window.setHtml("""<h4>An internal error was encountered loading the quick start guide. 
+                Please report this error to the developers.""")
 
-        # font = logOutput.font()
-        # font.setFamily("Courier")
-        # font.setPointSize(10)
-
-        layout.addWidget(logOutput)
+        layout.addWidget(display_window)
 
         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok)
         buttonBox.accepted.connect(self.accept)
 
         layout.addWidget(buttonBox)
-
         self.setLayout(layout)
+
+        # Move the dialog to be offset from the main window so the user can see the 'Add Account' button
+        # while viewing the guide.
+        y = parent.geometry().y()   # use parent's y position
+        self.move(self.pos().x() + 250, y)  # offset from original x position
+
 
     def load_quickstart_text(self):
         dir_path = "docs"
@@ -41,6 +45,7 @@ class QuickStartDialog(QDialog):
             base_dir = sys._MEIPASS  # type: ignore   #--keep PyCharm happy
             dir_path = os.path.join(base_dir, "assets")
         self.file_path = os.path.join(dir_path, 'quick_start_guide.html')
+        self.logger.debug(f"file_path: {self.file_path}")
         try:
             f = open(self.file_path)
             text = f.read()
@@ -72,3 +77,15 @@ class YourDialog(QDialog):
 # parent_window = YourParentWindow()
 # dialog = YourDialog(parent=parent_window)
 # dialog.exec_()"""
+
+# To include images in the guide they must be local: QTextEdit can't get remote URLs.
+#display_window.setHtml(f"""
+#    <h2>Quick Start Guide</h2>
+#    <img src="file:///{image_path}" alt="TOTP code">
+#""")
+# image_path must be adjusted for pyinstaller
+
+# To adjust the font size:
+# font = display_window.font()
+# font.setFamily("Courier")
+# font.setPointSize(10)
